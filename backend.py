513cdb9f918a6fb3b8a4a4f8fe539afd289d2f24
gruand <gruand@gmail.com>
2024-03-15 16:58:00 +0200

    Update openai client with new methods, working on model gpt-3.5-turbo

--- backend.py
+++ backend.py
@@ -1,20 +1,29 @@
+# Working on gpt-3.5-turbo
+
 import openai
+from openai import OpenAI
+import os
 
 
 class Chatbot():
     def __init__(self):
-        openai.api_key = "sk-***"
+        openai.api_key = os.environ["OPENAI_API_KEY"]
 
     def get_response(self, user_input):
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_input,
-            max_tokens=3000,
-            temperature=0.5
-        ).choice[0].text
-        return response
+        completion = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input,
+                },
+            ],
+        )
+        result = completion.choices[0].message.content
+        return result
+
 
 if __name__ == "__main__":
     chatbot = Chatbot()
-    response = chatbot.get_response("Write joke about dolphins")
+    response = chatbot.get_response("Write joke about whiskey")
     print(response)
