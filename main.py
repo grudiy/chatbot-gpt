e2ee3d37ac43ca29bfc187faff477c1cb14c64d7
gruand <gruand@gmail.com>
2024-03-15 17:41:34 +0200

    Implement multi threading my questions and bot's answer.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import threading
 
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication
 from backend import Chatbot
@@ -19,6 +20,8 @@ class ChatBotWindow(QMainWindow):
         # Input field widget
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10, 500, 640, 40)
+        # Send input by Enter
+        self.input_field.returnPressed.connect(self.send_message)
 
         # Button widget
         self.button = QPushButton("Send", self)
@@ -36,13 +39,15 @@ class ChatBotWindow(QMainWindow):
         self.chat_area.append("\n")
         self.input_field.clear()
 
-        # Send message to the chat GPT
+        # Send message to the chat GPT in separate thread, not to slow down the process
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
         response = self.chatbot.get_response(user_input)
-
         # Display response in chat area
-        self.chat_area.append(f"ChatGPT 3.5: {response}")
+        self.chat_area.append(f"<p style='color:#333333; background-color:#E9E9E9'> ChatGPT 3.5: {response}</p>")
         self.chat_area.append("\n")
-
 
 
 app = QApplication(sys.argv)
